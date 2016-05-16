8deed1f11d01e80e874d28a0285cdd5dc107f0ec
Paul Dilyard <paul.dilyard@gmail.com>
2016-05-16 13:05:56 -0400

    Create release 1.3.0
    - Includes support for `[set: {:binary, <<1, 2>>}]` thanks to @bakkdoor

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExOrient.Mixfile do
 
   def project do
     [app: :ex_orient,
-     version: "1.2.1",
+     version: "1.3.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
